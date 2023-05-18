7c51f12c5006d2e28aa79908099f562aed17c2df
Mic Neale <michael.neale@gmail.com>
2023-05-18 19:46:14 +1000

    starting with web5

--- main.py
+++ main.py
@@ -9,26 +9,31 @@ app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.c
 # Keep track of todo's. Does not persist if Python session is restarted.
 _TODOS = {}
 
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
-    return quart.Response(response='OK', status=200)
 
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
+@app.get("/help/setup")
+async def help_setup():
+    return quart.Response(response=json.dumps({"code": "npm install @tbd54566975/web5", "explanation": "This is how you install it from the command line."}), status=200)
 
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
-    request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
-    # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
-    return quart.Response(response='OK', status=200)
+
+@app.get("/help/did")
+async def help_did():
+    return quart.Response(response=json.dumps({"code": "const did = await web5.did.create('ion');", "explanation": "Create a Decentralized Identifier (DID). This one creates one of the ION did method type."}), status=200)
+
+
+store = '''
+        const { record } = await web5.dwn.records.write(did.id, {
+          author: did.id,
+          data: "Hello Web5",
+          message: {
+            dataFormat: 'text/plain',
+          },
+        });
+'''
+
+@app.get("/help/store")
+async def help_store():
+    return quart.Response(response=json.dumps({"code": store, "explanation": "Shows how to store in the web5 DWN."}), status=200)
+
 
 @app.get("/logo.png")
 async def plugin_logo():
