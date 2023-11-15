5380e3bee6b6f1ece5d3877bd9a5290beebcf028
Mic Neale <michael.neale@gmail.com>
2023-11-15 16:29:46 +1100

    adding in assistant api indexer

---
+++ indexer/README.md
@@ -0,0 +1,1 @@
+Creates an assistant in openai - as an experiment.
---
+++ indexer/indexer.js
@@ -0,0 +1,81 @@
+import Sitemapper from 'sitemapper';
+import OpenAI from "openai";
+import { JSDOM } from 'jsdom' 
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+
+const openai = new OpenAI();
+
+async function fullWebsiteAssistant(url) {
+  const sitemap = new Sitemapper({ url });
+  const { sites } = await sitemap.fetch();
+
+  // Fetch content from each site
+  const siteContents = await Promise.all(sites.map(async (site) => {
+    
+    const response = await fetch(site);
+    const respBody = await response.text()
+    const dom = new JSDOM(respBody);
+    
+    const textContent = dom.window.document.body.textContent || "";
+
+    return textContent; // Assuming the content is text
+  }));
+
+  // Determine the number of contents per bucket
+  const contentsPerBucket = Math.ceil(siteContents.length / 20);
+  
+  // Create and upload buckets
+  const files = [];
+  for (let i = 0; i < siteContents.length; i += contentsPerBucket) {
+    // Combine contents in each bucket
+    const tempDir = os.tmpdir();
+    const filePath = path.join(tempDir, `bucket_${i}.txt`);
+    const combinedContent = siteContents.slice(i, i + contentsPerBucket).join("\n");
+    fs.writeFileSync(filePath, combinedContent);
+
+    // Upload the File-like object
+    const file = await openai.files.create({
+      file: fs.createReadStream(filePath),
+      purpose: "assistants",
+    });
+    console.log("Created file: " + file.id);
+    files.push(file);
+    // write to disk
+    fs.writeFileSync(`bucket_${i}.txt`, combinedContent);
+  }
+
+
+
+
+
+  console.log("Creating assistant on files: " + files.length);
+
+  return openai.beta.assistants.create({
+    instructions: "You are an assistant that answers questions and provides code examples for decentralized web tech including web5, VCs and DIDs. Prefer the content provided where possible over general knowledge.",
+    model: "gpt-4-1106-preview",
+    tools: [{ "type": "retrieval" }],
+    file_ids: files.map(file => file.id)
+  });
+}
+
+
+await deleteAllFiles()
+await fullWebsiteAssistant('https://developer.tbd.website/sitemap.xml')
+
+async function deleteAllFiles() {
+    let files = await openai.files.list()
+    console.log(files)
+    // delete all files
+    for (const file of files.data) {
+        console.log("Deleting file: " + file.id)
+        await openai.files.del(file.id)
+    }
+}
+
+
+
+//files = await openai.files.list()
+//console.log(files)
+
